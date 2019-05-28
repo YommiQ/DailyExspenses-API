f3af25160bad541628a2752d7835ed4b32c45d94
YommiQ <yommiq@gmail.com>
2019-05-28 15:35:44 +0300

    IUserRepository + User entity

--- DailyExpenses.Domain/Entities/User.cs
+++ DailyExpenses.Domain/Entities/User.cs
@@ -6,5 +6,11 @@ namespace DailyExpenses.Domain.Entities
 {
     public class User
     {
+        public string Email { get; set; }
+        public string Login { get; set; }
+        public bool IsActive { get; set; }
+
+        public byte[] PasswordSalt { get; set; }
+        public byte[] PasswordHash { get; set; }
     }
 }
--- DailyExpenses.Domain/IRepositories/IUserRepository.cs
+++ DailyExpenses.Domain/IRepositories/IUserRepository.cs
@@ -1,11 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using DailyExpenses.Domain.Entities;
+using System;
 
 namespace DailyExpenses.Domain.IRepositories
 {
     public interface IUserRepository
     {
-
+        User GetById(Guid id);
+        User GetByLoginOrEmail(string value);
+        void Create(User user);
     }
 }
